771c47eb4c7ab4d2ac7a9fb256298fd0725cc2e3
MikeBoguszewski <133729602+MikeBoguszewski@users.noreply.github.com>
2025-02-07 16:52:32 -0600

    Style login page

--- app/components/LoginForm.jsx
+++ app/components/LoginForm.jsx
@@ -28,18 +28,32 @@ export default function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} noValidate>
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" required onChange={(e) => setEmail(e.target.value)} />
-      <label htmlFor="password">Password</label>
-      <input type="password" id="password" name="password" required onChange={(e) => setPassword(e.target.value)} />
-      {error && <p>{error}</p>}
-      <button type="submit">Log In</button>
-      <button type="button" onClick={handleAnonymousLogin}>
-        Demo Account
+    <form onSubmit={handleSubmit} noValidate className="bg-federal-blue p-14 rounded-2xl shadow-2xl flex flex-col md:w-96">
+      <h1 className="font-bold text-4xl mb-8">Login</h1>
+      <div className="flex flex-col mb-4">
+        <label htmlFor="email" className="font-bold">
+          Email
+        </label>
+        <input type="email" id="email" name="email" required onChange={(e) => setEmail(e.target.value)} className="rounded-md bg-inherit shadow-md border border-white p-1 outline-non-photo-blue mb-3" />
+      </div>
+      <div className="flex flex-col mb-4">
+        <label htmlFor="password" className="font-bold">
+          Password
+        </label>
+        <input type="password" id="password" name="password" required onChange={(e) => setPassword(e.target.value)} className="rounded-md bg-inherit shadow-md border border-white p-1 outline-non-photo-blue mb-3" />
+        {error && <p>{error}</p>}
+      </div>
+      <button type="submit" className="font-bold block bg-pacific-cyan flex-grow rounded-md shadow-md p-2 mb-3 hover:bg-vivid-sky-blue">
+        Log In
+      </button>
+      <button type="button" onClick={handleAnonymousLogin} className="font-bold block bg-honolulu-blue flex-grow rounded-md shadow-md p-2 mb-3 hover:bg-vivid-sky-blue">
+        Continue as Guest
       </button>
       <span>
-        Don't have an account? <Link href={"/signup"}>Sign Up.</Link>
+        Don't have an account?{" "}
+        <Link href={"/signup"} className="font-bold hover:underline">
+          Sign Up.
+        </Link>
       </span>
     </form>
   );
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,9 @@
 import LoginForm from "@/components/LoginForm";
+import Logo from "../components/Logo";
 export default function LoginPage() {
   return (
-    <div>
+    <div className="flex flex-col items-center md:flex-row md:justify-center min-h-screen w-full lg:gap-32">
+      <Logo />
       <LoginForm />
     </div>
   );
